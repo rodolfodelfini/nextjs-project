bb64534d34f2ad78384233f3fee8abc6801dea25
rodolfodelfini <rodolfodelfini@gmail.com>
2021-02-12 20:07:19 -0300

    principal

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
-function Home(){
-    return <div>Home teste 22 </div>
-}
+import React, {Component, useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default Home
+
+const Index = ({dados}) => (
+    <div>
+        <h1>Test NextJs + React + Vercel</h1> 
+        {console.log(dados)}
+        <ul>
+            {dados.map(registro =>(
+                <li key={registro.id}>
+                    <h2>Name:{registro.name}</h2>
+                    <p>Website:{registro.website}</p>
+                </li>
+            ))}
+        </ul>
+    </div>
+);
+   
+Index.getInitialProps = async () =>{
+    const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users/'
+    );
+
+    return { dados: response.data}
+};
+
+
+
+export default Index
