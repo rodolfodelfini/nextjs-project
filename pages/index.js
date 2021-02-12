4460d1d0e12bdb3bde673a0655abcee9debb0f10
rodolfodelfini <rodolfodelfini@gmail.com>
2021-02-12 20:33:45 -0300

    principal

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,30 @@
 import React, {Component, useState, useEffect } from 'react';
 import axios from 'axios';
 
-
 const Index = ({dados}) => (
     <div>
         <h1>Test NextJs + React + Vercel</h1> 
         {console.log(dados)}
         <ul>
             {dados.data.map(registro =>(
-                <li  key={registro.id}>
-                    <h2>Name:{registro.first_name}</h2>
-                    <p>Email:{registro.email}</p>
+                <li className="container" key={registro.id}>
+                    <h2>{registro.first_name}</h2>
+                    <p >{registro.email}</p>
                     <img src={registro.avatar}/>
                 </li>
             ))}
         </ul>
+        <style jsx>{`
+        .container {
+          margin: 30px;
+          float:left;
+        }
+        p {
+          color: blue;
+        }
+      `}</style>
     </div>
+    
 );
    
 Index.getInitialProps = async () =>{
@@ -25,7 +34,5 @@ Index.getInitialProps = async () =>{
 
     return { dados: response.data}
 };
-
-
 
 export default Index
